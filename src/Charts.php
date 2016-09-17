5804eff1606bae7b478d10351c0402bd6506152c
giacomo <ggrande4@gmail.com>
2016-09-17 18:34:55 +0200

    '.'

---
+++ src/Charts.php
@@ -0,0 +1,22 @@
+<?php
+namespace Autographics\Highcharts;
+
+class Charts
+{
+    /**
+     * Constructor
+     * @param 
+     */
+    public function __construct()
+    {
+        
+    }
+
+    /**
+     * @return boolean
+     */
+    public function test()
+    {
+        return true;
+    }
+}
